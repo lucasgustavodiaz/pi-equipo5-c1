8f2733c66377e15e89ae60688cf61ce6697a9b37
Cmaxi99 <maxicanto2009@hotmail.com>
2023-08-04 00:33:55 -0300

    Se agrego tabla y se le dio funcionalidad al boton agregar

--- app/administracion/registrar/page.jsx
+++ app/administracion/registrar/page.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
+import { Form } from "./form"
 
 export default function Page() {
-
-
     return (
         <>
             <h1>Acá registramos los productos</h1>
             <Link href={'/administracion'}>
-                Volver
+                    Volver
             </Link>
+            <Form />
         </>
         )
 }
