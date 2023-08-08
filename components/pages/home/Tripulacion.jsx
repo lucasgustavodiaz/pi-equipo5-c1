61ec8de2fe0bd71e93fe063ce27761329afd01bd
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-08-08 19:20:53 -0300

    card title

--- components/pages/home/Tripulacion.jsx
+++ components/pages/home/Tripulacion.jsx
@@ -7,7 +7,7 @@ export default function Tripulacion() {
     <div className='bg-[#f2f5fa]'>
       <div className='container py-20 text-center'>
         <GiShipWheel className='m-auto mb-4 text-4xl text-[#dbdfe4]' />
-        <h3 className='font-autography text-5xl text-sky-500  '>explorá</h3>
+        <h3 className='font-autography text-5xl text-sky-500  '>equipo</h3>
         <h2 className='mb-7 text-3xl font-bold text-sky-950 sm:text-5xl'>
           Conocé nuestros capitanes
         </h2>
