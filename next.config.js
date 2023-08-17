e249eaf57c269cdc2db4bcec9d92a32e0fc39c98
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-08-17 18:54:31 -0300

    vercel http CPS

--- next.config.js
+++ next.config.js
@@ -1,12 +1,34 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests'
+          }
+        ],
+
+        source: '/administracion', // Cambia esto a tus rutas específicas
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests'
+          }
+        ]
+      }
+    ]
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'g5bucket-c5.s3.us-east-2.amazonaws.com',
         port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+        pathname: '/**'
+      }
+    ]
+  }
+}
