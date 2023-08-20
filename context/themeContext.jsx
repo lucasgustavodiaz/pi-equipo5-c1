0ef06625a5c0f1d03a345582293a87e059f3c914
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-08-20 20:51:37 -0300

    not requiered

--- context/themeContext.jsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { ThemeProvider } from 'next-themes'
-
-export default function ThemeProviderWrapper({ children }) {
-  return <ThemeProvider attribute='class'>{children}</ThemeProvider>
-}
