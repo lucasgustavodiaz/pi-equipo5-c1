e818d34ce4fff38116fd3ec15807f53a981f1045
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-08-21 12:53:41 -0300

    menu terminado

---
+++ components/header/Dropdown.jsx
@@ -0,0 +1,56 @@
+import { useAuth } from '@/context/authContext'
+import Link from 'next/link'
+import { BsGear } from 'react-icons/bs'
+import { BiLogOut } from 'react-icons/bi'
+import { AiOutlineHome } from 'react-icons/ai'
+import { MdOutlineAdminPanelSettings } from 'react-icons/md'
+import { useRouter } from 'next/navigation'
+
+export default function Dropdown({ user, openMenu }) {
+  const { logout } = useAuth()
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    try {
+      await logout()
+      router.push('/')
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  return (
+    <div
+      className={`absolute right-0 top-12 flex w-[200px] flex-col rounded-xl bg-slate-50 p-2 text-sm drop-shadow-lg ${
+        openMenu ? 'block' : 'hidden'
+      }`}
+    >
+      <div className='cursor-pointer rounded-lg p-3 font-semibold hover:bg-slate-200'>
+        <div>Signed in as</div>
+        <div className='truncate'>{user.email}</div>
+      </div>
+      <Link href='/' className='rounded-lg px-3 py-2 hover:bg-slate-200'>
+        <AiOutlineHome className='mr-2 inline-block text-base' />
+        Inicio
+      </Link>
+      <Link href='/' className='rounded-lg px-3 py-2 hover:bg-slate-200'>
+        <BsGear className='mr-2 inline-block text-base' />
+        Configuración
+      </Link>
+      <Link
+        href='/administracion'
+        className='rounded-lg px-3 py-2 hover:bg-slate-200'
+      >
+        <MdOutlineAdminPanelSettings className='mr-2 inline-block text-base' />
+        Administración
+      </Link>
+      <button
+        onClick={handleLogout}
+        className='rounded-lg px-3 py-2 text-left hover:bg-rose-200 hover:text-pink-600 '
+      >
+        <BiLogOut className='mr-2 inline-block text-base' />
+        Cerrar sesión
+      </button>
+    </div>
+  )
+}
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -7,21 +7,29 @@ import logoDark from '/public/logo-dark.svg'
 import ThemeButton from '@/components/header/ThemeButton'
 import Hamburguer from '@/components/header/Hamburguer'
 import { useAuth } from '@/context/authContext'
-import { useRouter } from 'next/navigation'
+import Dropdown from './Dropdown'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
-  const { logout, user, loading } = useAuth()
-  const router = useRouter()
+  const { user, loading } = useAuth()
+
   // console.log(user)
 
-  const handleLogout = async () => {
-    try {
-      await logout()
-      router.push('/')
-    } catch (error) {
-      console.error(error.message)
+  const [openMenu, setOpenMenu] = useState(false)
+
+  useEffect(() => {
+    const closeMenu = () => {
+      setOpenMenu(false)
     }
-  }
+
+    if (openMenu) {
+      document.addEventListener('click', closeMenu)
+
+      return () => {
+        document.removeEventListener('click', closeMenu)
+      }
+    }
+  }, [openMenu])
 
   return (
     <header className='sticky top-0 z-50 bg-white py-3 dark:bg-[#121212]'>
@@ -48,33 +56,31 @@ export default function Header() {
             </Link>
           </li>
         </ul>
-        <div className='flex items-center gap-4'>
+        <div className='relative flex items-center gap-4'>
           {user ? (
             <>
-              <h2 className='text-md hidden font-medium tracking-tight sm:inline-block'>
-                {user.email}
-              </h2>
-              {user.photoURL ? (
-                <Image
-                  src={user.photoURL}
-                  alt='Picture of the user'
-                  width={44}
-                  height={44}
-                  className='rounded-full border-2 border-sky-500'
-                />
-              ) : (
-                <div className='flex h-[44px] w-[44px] items-center justify-center rounded-full border-2 border-sky-500 bg-gradient-to-r from-cyan-500 to-blue-500'>
-                  <p className='font-bold uppercase text-white'>
-                    {user.email[0]}
-                  </p>
-                </div>
-              )}
-              <button
-                onClick={handleLogout}
-                className='hidden rounded border border-sky-500 bg-transparent px-4 py-2 font-semibold text-sky-700 transition ease-in-out hover:border-transparent hover:bg-sky-500 hover:text-white dark:text-white sm:block'
+              {/* Contenido para el usuario autenticado */}
+              <div
+                onClick={() => setOpenMenu(!openMenu)}
+                className='cursor-pointer '
               >
-                Salir
-              </button>
+                {user.photoURL ? (
+                  <Image
+                    src={user.photoURL}
+                    alt='Picture of the user'
+                    width={44}
+                    height={44}
+                    className='rounded-full border-2 border-sky-500'
+                  />
+                ) : (
+                  <div className='flex h-[44px] w-[44px] cursor-pointer items-center justify-center rounded-full border-2 border-sky-500 bg-gradient-to-r from-cyan-500 to-blue-500'>
+                    <p className='font-bold uppercase text-white'>
+                      {user.email[0]}
+                    </p>
+                  </div>
+                )}
+              </div>
+              <Dropdown user={user} openMenu={openMenu} />
             </>
           ) : (
             !loading && (
@@ -95,7 +101,7 @@ export default function Header() {
           )}
 
           {/* <ThemeButton /> */}
-          <Hamburguer />
+          {/* <Hamburguer /> */}
         </div>
       </nav>
     </header>
